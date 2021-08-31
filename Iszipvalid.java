070674479a0c5c18550c44322126065161a0c570
Divyajyothi2210 <88102484+Divyajyothi2210@users.noreply.github.com>
2021-08-31 13:43:26 +0530

    spring mvc assignment

---
+++ Iszipvalid.java
@@ -0,0 +1,16 @@
+import java.lang.annotation.*;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Constraint(validatedBy= ZipValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface Iszipvalid {
+String message() default "Zipcode does not belong to the city selected or it is invalid";
+	
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
